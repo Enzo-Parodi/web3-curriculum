b62f005150497036b008940743fe39a01c49c74c
Enzo Parodi <enzoparodi90@gmail.com>
2024-10-20 10:30:54 -0400

    Finish progress first course

---
+++ learn-digital-ledgers-by-building-a-blockchain/add-block.js
@@ -0,0 +1,16 @@
+import { getTransactions, writeTransactions, getBlockchain, writeBlockchain } from "./blockchain-helpers.js";
+
+const blockchain = getBlockchain()
+const previousBlock = blockchain[blockchain.length - 1]
+const transactions = getTransactions()
+const newBlock = {
+    hash: Math.random().toString(),
+    previousHash: previousBlock.hash,
+    transactions
+}
+
+blockchain.push(newBlock)
+
+writeBlockchain(blockchain)
+writeTransactions([])
+
